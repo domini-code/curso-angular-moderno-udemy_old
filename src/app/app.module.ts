4dd03690c46de82e352c8ea761532be10eaf9dc5
bezael <bezael@gmail.com>
2024-07-25 08:28:04 +0200

    converte to old way

---
+++ src/app/app.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { RouterOutlet } from '@angular/router';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, RouterOutlet],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
