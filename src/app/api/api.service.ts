82b5386fb00826fb24ea64cf04c44c31c4567c10
bezael <bezael@gmail.com>
2024-08-20 09:34:30 +0200

    add crud to service

--- src/app/api/api.service.ts
+++ src/app/api/api.service.ts
@@ -6,9 +6,25 @@ import { Product } from '../features/products/models/product.model';
 @Injectable({ providedIn: 'root' })
 export class APIService {
   private readonly _httpClient = inject(HttpClient);
-  private readonly _MyURL = 'https://fakestoreapi.com/products';
+  private readonly _myURL = 'https://fakestoreapi.com/products';
 
   getProducts(): Observable<Product[]> {
-    return this._httpClient.get<Product[]>(this._MyURL);
+    return this._httpClient.get<Product[]>(this._myURL);
+  }
+
+  getProductById(id: number): Observable<Product> {
+    return this._httpClient.get<Product>(`${this._myURL}/${id}`);
+  }
+
+  createProduct(product: Product): Observable<Product> {
+    return this._httpClient.post<Product>(this._myURL, product);
+  }
+
+  updateProduct(id: number, product: Product): Observable<Product> {
+    return this._httpClient.put<Product>(`${this._myURL}/${id}`, product);
+  }
+
+  deleteProduct(id: number): Observable<any> {
+    return this._httpClient.delete(`${this._myURL}/${id}`);
   }
 }
